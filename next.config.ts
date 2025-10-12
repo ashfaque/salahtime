a7c5c88dbf40c1edc6fde3ca8c16e2e76818fa40
Ashfaque Alam <67138411+ashfaque@users.noreply.github.com>
2025-10-12 21:12:36 +0530

    changed css to scss

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // output: "export",  // Enable static export for GitHub Pages like Angular builds.
 };
 
 export default nextConfig;

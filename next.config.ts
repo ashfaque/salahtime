4438fc319a8ae053c0a1a526d93949e73d48421a
Ashfaque Alam <67138411+ashfaque@users.noreply.github.com>
2026-01-04 16:27:31 +0530

    Updated global.scss for Tailwind v4 standards, also updated next.config.ts to be compatible with GitHub Pages.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,25 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "";
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, "");
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig: NextConfig = {
   /* config options here */
   // output: "export",  // Enable static export for GitHub Pages like Angular builds.
+  output: isGithubActions ? "export" : undefined,
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 };
 
 export default nextConfig;
